0d54bf0b289c560b516162ee5a30665b3da07a3f
jruiz <joaquin@money-go.es>
2023-03-07 08:33:28 +0100

    -

--- app/src/main/java/com/example/testuploadgradlefirebase/MainActivity.kt
+++ app/src/main/java/com/example/testuploadgradlefirebase/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.testuploadgradlefirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text).setText("${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})")
     }
 }
